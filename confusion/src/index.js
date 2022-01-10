c4a289de7149f2124236afff5ee26e02715fd992
Huynh Chi Bao <baohc110902@gmail.com>
2022-01-10 23:47:51 +0700

    lab02_2

--- confusion/src/index.js
+++ confusion/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+yarn add react-popper@0.9.2
 
 ReactDOM.render(
   <React.StrictMode>
